a71977e7abad73df5251a483f28ff9c09b7a7eb0
maxykoin <86720966+maxykoin@users.noreply.github.com>
2024-02-22 11:43:04 -0300

    220224

--- atividades/a1at2b.py
+++ atividades/a1at2b.py
@@ -1,12 +1,13 @@
 # Faça um programa que calcule as raízes de um polinômio de segunda ordem via fórmula de Bhaskara. Considere que as raízes sejam reais.
-import math
+import math # importa a biblioteca integrada do python (numpy não rodou)
 
+# atribui os valores do polinômio a variáveis
 a = float(input("Valor de A: "))
 b = float(input("Valor de B: "))
 c = float(input("Valor de C: "))
 
-delta = (b**2) - (4*a*c)
-x1 = (-b + math.sqrt(delta)) / (2*a)
-x2 = (-b - math.sqrt(delta)) / (2*a)
+delta = (b**2) - (4*a*c) # calcula o delta
+x1 = (-b + math.sqrt(delta)) / (2*a) # calcula a primeira raiz fazendo uso da função de raiz quadrada da biblioteca
+x2 = (-b - math.sqrt(delta)) / (2*a) # calcula a segunda raiz fazendo uso da função de raiz quadrada da biblioteca
 
-print(f"As raízes dessa equação de segundo grau é de {x1} e {x2}")
+print(f"As raízes dessa equação de segundo grau é de {x1} e {x2}") # print dos resultados
