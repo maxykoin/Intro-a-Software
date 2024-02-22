d1a67322a403a5b951ac5b348a934d8a88b83e95
maxykoin <86720966+maxykoin@users.noreply.github.com>
2024-02-22 11:40:28 -0300

    Add files via upload

--- atividades/a1at2b.py
+++ atividades/a1at2b.py
@@ -1,2 +1,12 @@
 # Faça um programa que calcule as raízes de um polinômio de segunda ordem via fórmula de Bhaskara. Considere que as raízes sejam reais.
-     
+import math
+
+a = float(input("Valor de A: "))
+b = float(input("Valor de B: "))
+c = float(input("Valor de C: "))
+
+delta = (b**2) - (4*a*c)
+x1 = (-b + math.sqrt(delta)) / (2*a)
+x2 = (-b - math.sqrt(delta)) / (2*a)
+
+print(f"As raízes dessa equação de segundo grau é de {x1} e {x2}")
