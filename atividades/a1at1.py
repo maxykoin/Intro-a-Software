1a64c3a5fa9afe94daaa12861431ad99c4749080
maxykoin <86720966+maxykoin@users.noreply.github.com>
2024-02-23 11:33:43 -0300

    Delete atividades directory

--- atividades/a1at1.py
+++
@@ -1,9 +0,0 @@
-# Com base no que foi apresentado anteriormente, crie um programa que você deve digitar os seguintes dados: nome, altura, peso e idade. 
-# O programa deve imprimir uma frase contendo estas informações.
-
-nome = input("Qual é seu nome? ") # pergunta o nome od usuário
-altura = int(input("Qual é sua altura? ")) # pergunta a altura do usuário e considera somente números inteiros
-peso = float(input("Qual é seu peso? ")) # pergunta o peso do usuário e considera números com virgula
-idade = int(input("Qual é sua idade? ")) # pergunta a idade do usuário e considera somente números inteiros
-
-print(f"Olá {nome}! Você mede {altura}cm, pesa {peso}Kg e tem {idade} anos")
