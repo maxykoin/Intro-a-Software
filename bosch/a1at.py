34f307de916e2f3dad37e080f05605159f7e4c35
maxykoin <86720966+maxykoin@users.noreply.github.com>
2024-02-27 16:48:32 -0300

    270224

--- bosch/a1at.py
+++ bosch/a1at.py
@@ -1,11 +1,11 @@
 # Celsius to fahrenheint
-# c = float(input("Insira a temperatura em celsius: "))
-# print(f"A temperatura {c}C é {(c * 9/5) + 32}F")
+c = float(input("Insira a temperatura em celsius: "))
+print(f"A temperatura {c}C é {(c * 9/5) + 32}F")
 
 # Primeiro e último ano
-# n = input("Qual é seu nome inteiro? ").split()
-# n1 = n[0], n[-1]
-# print(" ".join(n1))
+n = input("Qual é seu nome inteiro? ").split()
+n1 = n[0], n[-1]
+print(" ".join(n1))
 
 # 3 first letters of the name uppercase ooo
 n = input("Qual é seu nome? ")
--- random/test.java
+++ random/test.java
@@ -1,6 +1,6 @@
-public class test(){
-    public static class main(String[] arg){
-        firstMovie = new movie();
+public class test {
+    public static void main(String[] arg) {
+        movie firstMovie = new movie();
         firstMovie.name = "Tangled";
         firstMovie.release = 2010;
         firstMovie.runtime = 100;
@@ -10,27 +10,24 @@ public class test(){
         firstMovie.ratings(4);
 
         System.out.println(firstMovie.name);
-        System.out.println(sumRatings);
-        System.out.println(allRatings);
     }
 }
 
-private class movie{
+private class movie {
     String name;
     int release;
     int runtime;
     String director;
-    double allRantings;
+    double allRatings;
     double sumRatings;
     double grade;
 
-    
-    void ratings(int grade){
+    double ratings(int grade) {
         sumRatings += grade;
         allRatings++;
     }
 
-    double meanRatings(){
-        return sumRatings / allRantings;
+    double meanRatings() {
+        return sumRatings / allRatings;
     }
 }
